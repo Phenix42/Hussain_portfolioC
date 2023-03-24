1d5585cf436d9675ce0fe9a9b34951b23e2295ef
SK Hussain <64408513+Phenix42@users.noreply.github.com>
2023-03-24 17:56:45 +0530

    updated files

---
+++ src/portfolio.js
@@ -0,0 +1,469 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello.",
+  title2: "Hussain",
+  logo_name: "<Hussain/>",
+  nickname: "Hussain",
+  full_name: "SK Hussain",
+  subTitle:
+    "React Developer. Always learning.",
+  resumeLink:
+    "https://drive.google.com/file/d/1VffiZ6s8u80D5Af7TqG-AzIp3Bw1Fdal/view?usp=share_link",
+  mail: "sk.hussain018@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/Phenix42",
+  linkedin: "https://www.linkedin.com/in/sk-hussain-3417b8179/",
+  gmail: "sk.hussain018@gmail.com",
+  gitlab: " ",
+  facebook: " ",
+  twitter: "",
+  instagram: "https://www.instagram.com/18._phenixdorx_.10",
+};
+
+const skills = {
+  data: [
+    {
+      title: "React developer",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive website front end using ReactJS",
+        "⚡ Collaborating with the team to design and implement new features and functionalities",
+        "⚡ Creating UX/UI Desigen",
+      ],
+      softwareSkills: [
+        
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+      ],
+    },
+    {
+      title: "Front-End Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Designing and developing user interfaces using React JS and other web technologies.",
+        "⚡ Optimizing the application for maximum speed and scalability Staying up-to-date with emerging trends and technologies in web development",
+        "⚡ Continuously improving the application's performance and user experience.",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+       
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+
+ 
+        {
+          skillName: "Android App Development",
+          fontAwesomeClassname: "simple-icons:android",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+
+
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+      ],
+    
+    },
+    // {
+    //   title: "Cloud Infra-Architecture",
+    //   fileName: "CloudInfraImg",
+    //   skills: [
+    //     "⚡ Experience working on multiple cloud platforms",
+    //     "⚡ Experience hosting and managing websites",
+    //     "⚡ Deploying deep learning models on cloud to use on mobile devices",
+    //     "⚡ Experience with Continuous Integration",
+    //   ],
+    //   softwareSkills: [
+    //     {
+    //       skillName: "GCP",
+    //       fontAwesomeClassname: "simple-icons:googlecloud",
+    //       style: {
+    //         color: "#4285F4",
+    //       },
+    //     },
+    //     {
+    //       skillName: "AWS",
+    //       fontAwesomeClassname: "simple-icons:amazonaws",
+    //       style: {
+    //         color: "#FF9900",
+    //       },
+    //     },
+    //     {
+    //       skillName: "Netlify",
+    //       fontAwesomeClassname: "simple-icons:netlify",
+    //       style: {
+    //         color: "#38AFBB",
+    //       },
+    //     },
+    //     {
+    //       skillName: "Heroku",
+    //       fontAwesomeClassname: "simple-icons:heroku",
+    //       style: {
+    //         color: "#6863A6",
+    //       },
+    //     },
+    //     {
+    //       skillName: "Firebase",
+    //       fontAwesomeClassname: "simple-icons:firebase",
+    //       style: {
+    //         color: "#FFCA28",
+    //       },
+    //     },
+    //     {
+    //       skillName: "MongoDB",
+    //       fontAwesomeClassname: "simple-icons:mongodb",
+    //       style: {
+    //         color: "#47A248",
+    //       },
+    //     },
+    //     {
+    //       skillName: "Docker",
+    //       fontAwesomeClassname: "simple-icons:docker",
+    //       style: {
+    //         color: "#1488C6",
+    //       },
+    //     },
+    //   ],
+    // },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "JNTUK-University college of engineering Vizinagaram",
+      subtitle: "B.Tech in Computer Science Engineer",
+      logo_path: "JNTU-GV_Logo.png",
+      alt_name: "JNTUK",
+      duration: "2017 - 2021",
+      descriptions: [
+        "⚡ I have studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
+        "⚡ I have also completed various online courses for React, Web Development, Mobile App Development, etc.",
+        "⚡ I have implemented several projects based on what I've learnt under my Computer Engineering course. ",
+      ],
+      website_link: "https://jntukucev.ac.in/",
+    },
+    {
+      title: "Kendriya Vidyalaya - Waltair",
+      subtitle: "MPC with computer science",
+      logo_path: "KVLogoRound.png",
+      alt_name: "KV",
+      duration: "2015 - 2017",
+      descriptions: [
+        "⚡ I have studied core subjects like Maths physics and Chemistry with Computer science.",
+        "⚡ I have learned Computer science basic like c++,Sql.",
+        "⚡ I have implemented Basic projects like Tic tac toe. ",
+      ],
+      website_link: "https://waltair.kvs.ac.in/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Introduction to Operating system",
+      subtitle: "NPTEL",
+      logo_path: "NPTEL1.png",
+      certificate_link: " ",
+      alt_name: "NPTEL",
+      color_code: "#ffc475",
+      website_link: "",
+    },
+    {
+      title: "CCNAv7: Introduction to Networks",
+      subtitle: "Cisco Networking Academy",
+      logo_path: "Cisco.png",
+      certificate_link: " ",
+      alt_name: "Cisco Networking Academy",
+      color_code: "#FFFFFF",
+    },
+    {
+      title: "Python For everyone",
+      subtitle: "Udemy",
+      logo_path: "udemy.png",
+      certificate_link: " ",
+      alt_name: "udemy",
+      color_code: "#ffbfae",
+    },
+    {
+      title: "CPA: Programming Essentials in C++",
+      subtitle: "Cisco Networking Academy",
+      logo_path: "Cisco.png",
+      certificate_link: " ",
+      alt_name: "Cisco Networking Academy",
+      color_code: "#FFFFFF",
+    },
+    {
+      title: "Azure Fundamentals",
+      subtitle: "Microsoft",
+      logo_path: "Micro1.png",
+      certificate_link:
+        "https://www.credly.com/badges/15b652bb-988d-4442-ae1e-7cca588e0190?source=linked_in_profile",
+      alt_name: "Microsoft",
+      color_code: "#47A048",
+    },
+    {
+      title: "AWS Academy Cloud Foundations",
+      subtitle: "Amazon Web Services",
+      logo_path: "AWS.png",
+      certificate_link:
+        "https://www.credly.com/badges/f7057085-e1ca-4739-a341-8d4b4d9c95de",
+      alt_name: "Amazon Web Services",
+      color_code: "#F6B808",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I am working in Tcs and done two internships . I love to share my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work",
+      experiences: [
+        {
+          title: "React Developer",
+          company: "Tata Consultancy Services",
+          company_url: "https://www.tcs.com/",
+          logo_path: "TcsLogo.svg",
+          duration: "october 2021 - Present",
+          location: "Hyderabad",
+          description:
+            "Tata Consultancy Services is an Indian multinational information technology services and consulting company with its headquarters in Mumbai. It is a part of the Tata Group and operates in 150 locations across 46 countries. In July 2022, it was reported that TCS had over 600,000 employees worldwide.",
+          color: "#2962FF",
+        },
+      ],
+    },
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Cyber Security Intern",
+          company: "Indian Servers",
+          company_url: "https://indianservers.com/",
+          logo_path: "indianservers.jpg",
+          duration: "june 2020 - Aug 2020",
+          location: "Work From Home",
+          description:
+            "I have been working with tools which use to detect the vulnerable websites and Learned to prevent the cyber attcks on various devices ",
+          color: "#ee3c26",
+        },
+        {
+          title: "Web App Developer Intern",
+          company: "BalaSwecha",
+          company_url: " ",
+          logo_path: "BalaSwecha.png",
+          duration: "Aug 2020 - September 2020",
+          location: "Work From Home",
+          description:
+            "Internship task was to make a pencil Box application for ruler governament Schools. ",
+          color: "#0071C5",
+        },
+  
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "Inquiztive",
+          company: "JNTUK-UCEV",
+          // company_url: "https://sugarlabs.org/",
+          logo_path: "KVLogoRound.png",
+          duration: "Feb 2019",
+          location: " Vizinagaram",
+          description:
+            "Condected the Inquiztive tech quize in the fest event cresens 2k19",
+          color: "#4285F4",
+        },
+        {
+          title: "Genie",
+          company: " Cream Code Club",
+          company_url: " https://github.com/Cream-Code",
+          logo_path: "CreamCode.png",
+          duration: "2020",
+          location: " ",
+          description:
+            "Co-Founder and Member of cream code club which build search engine like Genie and App Geniefordrooid.",
+          color: "#D83B01",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Front_End devlopment projects. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "mypic.jpg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "My Vision",
+    subtitle:
+     " I believe that as a developer, my responsibility is not just to write code, but also to understand the needs of the end-users and create interfaces that solve their problems. My goal is to design and develop applications that not only meet but exceed the expectations of the users.",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      name: "Portfolio",
+      url: "https://github.com/Phenix42/Hussain_portfolio",
+      description:
+        "This is My Clean portfolio",
+      languages: [
+        {
+          name: "React JS",
+          iconifyClass: "logos-React",
+        },
+      ],
+    },
+    {
+      name: "Voice Based-Email",
+      url: "https://github.com/Phenix42/Voicemail-For-Blind.",
+      description: "Voice Based Email for blind",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+        {
+          name: "django",
+          iconifyClass: "logos-django",
+        },
+      ],
+    },
+    {
+      name: "Chit-Chat Application",
+      url: "https://github.com/Phenix42/ChitChat",
+      description: "This is a similar application like teams with the flexibilties of chatting.",
+      languages: [
+        {
+          name: "javascript",
+          iconifyClass: "logos-javascript",
+        },
+      ],
+    },
+    {
+      name: "Gene browsepad Application",
+      url: " https://github.com/Cream-Code/Gene-Browsepad",
+      description:
+        "Gene is a browse pad application like google search enhine.",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
